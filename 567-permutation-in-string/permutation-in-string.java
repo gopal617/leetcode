d56315034414e31bfe0eb643b55622d057a2aeeb
gopal617 <gopalkrishna5676@gmail.com>
2025-12-01 15:31:30 +0530

    Time: 6 ms (85.60%) | Memory: 43.8 MB (57.92%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,31 +1,20 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int sum1=0;
-        for(int i=0;i<s1.length();i++){
-            sum1+=(int) s1.charAt(i);
+    public boolean checkInclusion(String s, String p) {
+        int[] pc= new int[26];
+        int[] sc = new int[26];
+        for(char c: s.toCharArray()){
+                pc[c-'a']++;
         }
-        int sum2=0;
         int left=0;
-        StringBuilder s3= new StringBuilder();
-        for(int right=0;right<s2.length();right++){
-            sum2+=(int) s2.charAt(right);
-            s3.append(s2.charAt(right));
-            int c=s1.length();
-            if(right-left+1 == s1.length()){
-                if(sum1==sum2){
-                    String s4= s3.toString();
-                    for(int i=0;i<s1.length();i++){
-                        if (s4.contains(""+s1.charAt(i))){
-                            c--;
-                        }
-                        if (c==0){
-                            return true;
-                        }
-                    }
+        for(int right=0;right<p.length();right++){
+            sc[p.charAt(right)-'a']++;
+            if(right-left+1==s.length()){
+
+                if(Arrays.equals(pc,sc)){
+                    return true;
                 }
-                sum2-=(int) s2.charAt(left);
+                sc[p.charAt(left)-'a']--;
                 left++;
-                s3.deleteCharAt(0);
             }
         }
         return false;
