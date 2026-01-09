9b3d5370f0316a010d9a5ce5f4afae868f0e2747
gopal617 <gopalkrishna5676@gmail.com>
2026-01-09 14:50:27 +0530

    Time: 36 ms (89.73%) | Memory: 69.2 MB (76.27%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,33 +1,24 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-            int left = 0;
-        int maxLen = 0;
-
-        Map<Integer, Integer> map = new HashMap<>(); 
-
-        for (int right = 0; right < fruits.length; right++) {
-            map.put(fruits[right], right);
-            if (map.size() > 2) {
-
-                // Find fruit with smallest last index (oldest)
-                int minIndex = Integer.MAX_VALUE;
-                int fruitToRemove = -1;
-
-                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                    if(entry.getValue()<minIndex){
-                        minIndex = entry.getValue();
-                        fruitToRemove = entry.getKey();
-                
+        int left=0;
+        int max_=0;
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int right=0;right<fruits.length;right++){
+            m.put(fruits[right],right);
+            while(m.size()>2){
+                int minimumind=Integer.MAX_VALUE;
+                int removefruit=-1;
+                for(Map.Entry<Integer, Integer> e : m.entrySet()){
+                    if(minimumind>e.getValue()){
+                        minimumind=e.getValue();
+                        removefruit=e.getKey();
                     }
                 }
-
-                left = minIndex + 1;
-                map.remove(fruitToRemove);
+                left=minimumind+1;
+                m.remove(removefruit);
             }
-
-            maxLen = Math.max(maxLen, right - left + 1);
+            max_=Math.max(max_,right-left+1);
         }
-
-        return maxLen;
+        return max_;
     }
 }
