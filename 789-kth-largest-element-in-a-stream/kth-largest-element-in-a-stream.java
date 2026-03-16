da20559b0d844faedc1f9213ad8e8b31ffed6186
gopal617 <gopalkrishna5676@gmail.com>
2026-03-16 11:55:11 +0530

    Time: 21 ms (95.18%) | Memory: 52 MB (92.47%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -0,0 +1,28 @@
+class KthLargest {
+    PriorityQueue<Integer> p;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        p=new PriorityQueue<>();
+        for(int i:nums){
+            p.offer(i);
+            if(p.size()>k){
+                p.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        p.offer(val);
+        if(p.size()>k){
+            p.poll();
+        }
+        return p.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
