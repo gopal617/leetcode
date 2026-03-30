c0a77ac98272c107108eb37527d6d64862d2cd92
gopal617 <gopalkrishna5676@gmail.com>
2026-03-30 13:04:22 +0530

    Time: 5 ms (98.68%) | Memory: 48.1 MB (53.95%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+        int[] arr1= new int[26];
+        int[] arr2 =new int[26];
+        for(int i=0;i<s1.length();i++){
+            if(i%2==0){
+                arr1[s1.charAt(i)-'a']++;
+                arr1[s2.charAt(i)-'a']--;
+            }
+            else{
+                arr2[s1.charAt(i)-'a']++;
+                arr2[s2.charAt(i)-'a']--;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=0 || arr2[i]!=0) return false;
+        }
+        return true;
+    }
+}
