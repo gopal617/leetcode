58fac857856864f7a83ee58517624965b3c26946
gopal617 <gopalkrishna5676@gmail.com>
2026-04-04 00:34:28 +0530

    Time: 3 ms (72.63%) | Memory: 47.4 MB (43.85%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -13,45 +13,13 @@
 //  *     }
 //  * }
 //  */
-// class Solution {
-//     List<Integer> l = new ArrayList<>();
-//     public boolean findTarget(TreeNode root, int k) {
-//         if(dfs(root,l,k)== true){
-//             return true;
-//         }
-//         return false;
-        
-//     }
-//     private boolean dfs (TreeNode t,List<Integer> l,int k){
-//         if(t==null) return ;
-//         if(t.left==null && t.right==null) return ;
-//         if (l.contains(k-t.val)) return true;
-//         else {
-//             l.add(t.val);
-//         }
-//         else if(t.left!=null){
-//             dfs(t.left,l,k);
-//         } 
-//         else if(t.right!=null){
-//             dfs(t.right,l,k);
-//         }
-
-//     }
-// }
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
-
-    public boolean findTarget(TreeNode root, int k) {
-        // Base case: null node
-        if (root == null) return false;
-
-        // If complement is found, return true
-        if (set.contains(k - root.val)) return true;
-
-        // Otherwise, add current node value to the set
-        set.add(root.val);
-
-        // Recurse on left and right subtrees
-        return findTarget(root.left, k) || findTarget(root.right, k);
+    Set<Integer> l = new HashSet<>();
+    public boolean findTarget(TreeNode node, int k) {
+         if(node==null) return false;
+        if(l.contains(k-node.val)) return true;
+        l.add(node.val);
+        return (findTarget(node.left,k)||findTarget(node.right,k));
     }
+   
 }
