c7a1965b7346e6c8a9d601f5bd96b2946facf407
gopal617 <gopalkrishna5676@gmail.com>
2026-06-07 18:52:45 +0530

    Time: 19 ms (80.13%) | Memory: 45.7 MB (61.43%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
+        List<List<Integer>> l = new ArrayList<>();
         int n= nums.length;
-        List<List<Integer>> s = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1]){ continue;}
-            for(int j= i+1;j<nums.length;j++){
-                if(j!=i+1 && nums[j]==nums[j-1]){continue;}
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long sum=(long) nums[i]+nums[j]+nums[k]+nums[l];
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                int k = j+1;
+                int h=n-1;
+                while(k<h){
+                    long sum= (long) nums[i]+nums[j]+nums[k]+nums[h];
                     if(sum==target){
-                        s.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        List<Integer> sub = new ArrayList<>();
+                        sub.add(nums[i]);
+                        sub.add(nums[j]);
+                        sub.add(nums[k]);
+                        sub.add(nums[h]);
+                        l.add(sub);
+                        while(k<h && nums[k]==nums[k+1]) k++;
+                        while(k<h && nums[h]==nums[h-1]) h--;
                         k++;
-                        l--;
-                        while( k<l && nums[k]==nums[k-1]){k++;};
-                        while(k<l && nums[l]==nums[l+1]){l--;};
+                        h--;
                     }
-                    else if(sum<target){
-                        k++;
-                    }
-                    else{
-                        l--;
-                    }
-
+                    else if (sum<target) k++;
+                    else h--;
                 }
             }
-        }
-        return s;
+        } 
+        return l;
     }
 }
