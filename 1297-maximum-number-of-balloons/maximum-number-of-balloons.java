a8967660726087f51571cdaf118371191585c998
gopal617 <gopalkrishna5676@gmail.com>
2026-06-22 22:08:23 +0530

    Time: 2 ms (96.67%) | Memory: 43.2 MB (87.41%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,16 +1,25 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int[] arr = new int[26];
-        String b = "balon";
-        for(char c: text.toCharArray()){
-            arr[c-'a']++;
+        int[] balloon = new int[27];
+        for(char c : text.toCharArray()){
+            balloon[c-'a']++;
         }
-       
-        return  Math.min(
-            Math.min(arr['b' - 'a'], arr['a' - 'a']),
-            Math.min(
-                arr['n' - 'a'],
-                Math.min(arr['l' - 'a'] / 2, arr['o' - 'a'] / 2)
-            ));
+        if (balloon['l'-'a']<2 || balloon['o'-'a']<2) return 0;
+       int c=0;
+       while(balloon['b'-'a']!=0 && balloon['a'-'a']!=0 && balloon['l'-'a']!=0 && balloon['o'-'a']!=0  && balloon['n'-'a']!=0){
+        if(balloon['b'-'a']>=1 && balloon['a'-'a']>=1 && balloon['l'-'a']>=2 && balloon['o'-'a']>=2 && balloon['n'-'a']>=1){
+            c+=1;
+            balloon['b'-'a']-=1;
+            balloon['a'-'a']-=1;
+            balloon['l'-'a']-=2;
+            balloon['o'-'a']-=2;
+            balloon['n'-'a']-=1;
+        }
+        else{
+            balloon['l'-'a']=0;
+        }
+       }
+       return c;
+
     }
 }
